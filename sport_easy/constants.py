e788fef169282d7444de0251a0007bc15138e5c5
vaudrey750 <vaudrey750@gmail.com>
2025-08-24 16:45:22 +0200

    update

--- sport_easy/constants.py
+++ sport_easy/constants.py
@@ -10,3 +10,16 @@ PLAYER_MAPPING_POSITION = {
     'Attaquant': ['Attaquant', 4],
     'Ailier': ['Attaquant', 4]
 }
+
+
+COMPETITIONS = {
+    "2024-2025": {
+        "CDM R2": [1154627, 115],
+        "AMICAL": [1120616, 112],
+        "CDM COUPE PARIS CREDIT MUTUEL IDF": [1289465, 128],
+        "COUPE VDM CDM": [1328880, 132]
+    },
+    "2025-2026": {
+        "AMICAL": [1364019, 136]
+    }
+}
