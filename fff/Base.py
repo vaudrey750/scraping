58d2ea4b9b8a66a52f92ec33ab926a0b905a77a2
vaudrey750 <vaudrey750@gmail.com>
2025-10-26 18:23:02 +0100

    update

--- fff/Base.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-from bs4 import BeautifulSoup
-
-class BaseFffScraping:
-    def __init__(self, url: str):
-        self.page = get(url)
-        self.soup = BeautifulSoup(self.page.content, "lxml")
